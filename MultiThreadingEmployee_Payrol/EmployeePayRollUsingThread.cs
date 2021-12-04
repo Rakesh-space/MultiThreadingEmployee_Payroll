9a50216216951dd2d3108d48be05dc52979cb680
unknown <rakeshsmusale03@gmail.com>
2021-12-04 12:05:51 +0530

    Added Multiple Employee.

---
+++ MultiThreadingEmployee_Payrol/EmployeePayRollUsingThread.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadingEmployee_Payrol
+{
+    class EmployeePayRollUsingThread
+    {
+        public List<EmployeeDetails> employeeDetailsList = new List<EmployeeDetails>();
+        public void addEmployeeToPayroll(List<EmployeeDetails> employeeDetailsList)
+        {
+            employeeDetailsList.ForEach(employeeData =>
+            {
+                Console.WriteLine(" Employee being added: " + employeeData.EmployeeName);
+                this.addEmployeePayroll(employeeData);
+                Console.WriteLine(" Employee added: " + employeeData.EmployeeName);
+            });
+            Console.WriteLine(this.employeeDetailsList.ToString());
+        }
+
+        public void addEmployeePayroll(EmployeeDetails emp)
+        {
+            employeeDetailsList.Add(emp);
+        }
+    }
+}
--- MultiThreadingEmployee_Payrol/Program.cs
+++ MultiThreadingEmployee_Payrol/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MultiThreadingEmployee_Payrol
 {
@@ -6,7 +7,11 @@ namespace MultiThreadingEmployee_Payrol
     {
         static void Main(string[] args)
         {
+            List<EmployeeDetails> employeeDetailsList1 = new List<EmployeeDetails>();
             Console.WriteLine("Welcome to MultiThreading Employee_Payrol..!");
+
+            EmployeePayRollUsingThread employeePayRollUsingThread=new EmployeePayRollUsingThread();
+            employeePayRollUsingThread.addEmployeeToPayroll(employeeDetailsList1);
         }
     }
 }

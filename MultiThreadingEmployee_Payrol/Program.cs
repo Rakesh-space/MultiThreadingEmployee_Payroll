96842081bb8ac532a2feb0725a2b944f623c6d3b
unknown <rakeshsmusale03@gmail.com>
2021-12-04 11:46:58 +0530

    Welcome message.

---
+++ MultiThreadingEmployee_Payrol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiThreadingEmployee_Payrol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MultiThreading Employee_Payrol..!");
+        }
+    }
+}
